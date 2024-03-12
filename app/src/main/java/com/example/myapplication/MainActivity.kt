07dd5e0dc2dcfed2212fd9bc594964699b3ac9f2
not-known-person <ayushnigam843@gmail.com>
2024-03-12 10:34:58 -0400

    Added splash screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = Color.TRANSPARENT
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
     }
-    }
+}
